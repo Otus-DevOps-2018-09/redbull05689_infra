4193960044ee14543e74e352b9680807cda88d0a
Maxim Vasilev <redbull05689@gmail.com>
2018-11-03 00:44:22 +0300

    all vars done

--- terraform/db.tf
+++ terraform/db.tf
@@ -16,3 +16,15 @@ resource "google_compute_instance" "db" {
  ssh-keys = "appuser:${file(var.public_key_path)}"
  }
  }
+ resource "google_compute_firewall" "firewall_mongo" {
+ name = "allow-mongo-default"
+ network = "default"
+ allow {
+ protocol = "tcp"
+ ports = ["27017"]
+ }
+ # правило применимо к инстансам с тегом ...
+ target_tags = ["reddit-db"]
+ # порт будет доступен только для инстансов с тегом ...
+ source_tags = ["reddit-app"]
+ }
