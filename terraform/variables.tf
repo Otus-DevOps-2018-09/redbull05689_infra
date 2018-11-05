e0bf4a9014efbc16801fc31c2857ab2584d871b8
Maxim Vasilev <redbull05689@gmail.com>
2018-11-05 22:53:53 +0300

    Added vriables from terraform/modules/*/variables.tf to terraform/variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,5 +12,31 @@ variable instance_count {
   default     = "1"
 }
 
+variable public_key_path {
+ description = "Path to the public key used to connect to instance"
+ default = "~/.ssh/redbull05689.pub"
+}
+variable zone {
+ description = "Zone"
+ default = "europe-west1-b"
+}
+variable app_disk_image {
+ description = "Disk image for reddit app"
+ default = "reddit-app-base"
+}
 
+variable public_key_path {
+ description = "Path to the public key used to connect to instance"
+}
+variable zone {
+ description = "Zone"
+}
+variable db_disk_image {
+ description = "Disk image for reddit db"
+ default = "reddit-db-base"
+}
 
+variable source_ranges {
+  description = "Allowed IP addresses"
+  default     = ["0.0.0.0/0"]
+}
