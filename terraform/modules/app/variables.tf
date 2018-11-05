4347680ba32b2224cd7518b764c0f46486369f84
Maxim Vasilev <redbull05689@gmail.com>
2018-11-05 15:13:26 +0300

    checked some errores

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -1,8 +1,10 @@
 variable public_key_path {
  description = "Path to the public key used to connect to instance"
+ default = "~/.ssh/redbull05689.pub"
 }
 variable zone {
  description = "Zone"
+ default = "europe-west1-b"
 }
 variable app_disk_image {
  description = "Disk image for reddit app"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,32 +7,10 @@ variable region {
   default     = "europe-west1"
 }
 
-variable public_key_path {
-  description = "Path to the public key used for ssh access"
-}
-
-variable disk_image {
-  description = "Disk image"
-}
-
-variable zone {
-  description = "Zone there situable in"
-}
-
-variable private_key_path {
-  description = "Path to the private key used for ssh access"
-}
-
 variable instance_count {
   description = "Count of instances"
   default     = "1"
 }
 
-variable app_disk_image {
- description = "Disk image for reddit app"
- default = "reddit-app-base"
- }
- variable db_disk_image {
- description = "Disk image for reddit db"
- default = "reddit-db-base"
- }
+
+
