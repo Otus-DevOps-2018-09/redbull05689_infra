d79a48abed3de407e96c48873b815742ffb216e8
Maxim Vasilev <redbull05689@gmail.com>
2018-11-06 23:21:28 +0300

    Added prod/backend.tf

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -1,13 +1,4 @@
-/*
-module "storage-bucket" {
-  source  = "SweetOps/storage-bucket/google"
-  version = "0.1.1"
-  name    = ["storage-bucket-anton-iv-reddit"]
-}
-output storage-bucket_url {
-  value = "${module.storage-bucket.url}"
-}
-*/
+
 
 data "terraform_remote_state" "reddit" {
   backend = "gcs"
