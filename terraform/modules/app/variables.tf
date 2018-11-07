63ef88cd2868ed4f4654789fb7772f2a1db74fb0
Maxim Vasilev <redbull05689@gmail.com>
2018-11-07 16:48:51 +0300

    Added empty line in storage-bucket.tf

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -13,7 +13,7 @@ variable app_disk_image {
 
 variable private_key_path {
   description = "Path to the private key used for ssh access"
-  default     = "/home/redbull05689/.ssh/redbull05689"
+  default     = "~/.ssh/appuser"
   
 }
 variable db_ip {
